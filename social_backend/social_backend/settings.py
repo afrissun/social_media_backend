88d4ee9b7686e1cc66e3c5076cf6434b3273d6ef
Martin Joseph Lubowa <martinlubowa@outlook.com>
2024-01-09 16:48:22 +0300

    Added USE_S3

--- social_backend/social_backend/settings.py
+++ social_backend/social_backend/settings.py
@@ -175,6 +175,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+USE_S3 = os.getenv('USE_S3') == 'TRUE'
+
 if USE_S3:
     # aws settings
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
