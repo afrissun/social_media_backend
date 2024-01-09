fd25d7a57a2f32e2979f3fd4de19773b03958bf4
Martin Joseph Lubowa <martinlubowa@outlook.com>
2024-01-09 23:46:34 +0300

    Update settings.py

--- social_backend/social_backend/settings.py
+++ social_backend/social_backend/settings.py
@@ -189,17 +189,21 @@ if USE_S3:
     AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-     # s3 public media settings
+    # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'storage_backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 else:
+    # Use Whitenoise for serving static files in development
     STATIC_URL = 'static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
     MEDIA_URL = 'media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# ...
+
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 #MEDIA_URL = '/mediafiles/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
